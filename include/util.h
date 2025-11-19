edd703538cbbc06bcf98cfff88ea0bf9cc9d25bd
alonsopuente1 <freakingbosstv@gmail.com>
2025-11-19 02:07:44 +0000

    added comments

--- include/util.h
+++ include/util.h
@@ -4,13 +4,16 @@
 // goes through each element of the array and frees it
 // finally frees the array itself
 // ARRAY IS INVALID AFTER CALL TO THIS FUNCTION
+// safe to pass null
 extern void FreeDynamicArrayOfAllocatedElements(void** array, int numElements);
 
 // gets the index of the last appearance of chr in the str
 // if chr is not in string or NULL is passed to str, -1 is returned
 extern int GetLastChar(const char* str, char chr);
 
-// TODO: implement function and add list of maps on main menu screens
+// gets all the files in the dir matching the wildcard
+// for example 'images/*.png' will return an array
+// ["images/background.png", "images/brickwall.png", "images/grass.png"]
 extern char** GetAllFilesInDir(const char* dir, int* numFiles);
 
 #endif
